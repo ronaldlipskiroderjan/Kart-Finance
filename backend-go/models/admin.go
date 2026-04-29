5438edc1d851d72334f48c01db9c990bfe2ec949
ronaldlipskiroderjan <ronaldlipski1@gmail.com>
2026-04-29 15:39:20 -0300

    refactor: organiza modelos por domínio de dados (admin, pilot, finance)

---
+++ backend-go/models/admin.go
@@ -0,0 +1,8 @@
+package models
+
+type Admin struct {
+	ID       uint   `gorm:"primaryKey"`
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+}
