642c661b799962a4d96e1a9c65739c2728ef55fe
ronaldlipskiroderjan <ronaldlipski1@gmail.com>
2026-04-30 15:18:02 -0300

    feat:Adiciona a nova feature para acumular dividas não pagas

--- backend-go/dtos/closing_dto.go
+++ backend-go/dtos/closing_dto.go
@@ -5,7 +5,10 @@ type ClosingSummaryDTO struct {
 	BaseFee              float64 `json:"baseFee"`
 	TotalExpenses        float64 `json:"totalExpenses"`
 	TotalReimbursements  float64 `json:"totalReimbursements"`
-	TotalAmount          float64 `json:"totalAmount"`
-	Year                 int `json:"year"`
-	Month                int `json:"month"`
+	TotalAmount          float64 `json:"totalAmount"` // Valor deste mês apenas
+	PreviousDebt         float64 `json:"previousDebt"`
+	UnpaidMonthsCount    int     `json:"unpaidMonthsCount"`
+	FinalAmount          float64 `json:"finalAmount"` // totalAmount + previousDebt
+	Year                 int     `json:"year"`
+	Month                int     `json:"month"`
 }
